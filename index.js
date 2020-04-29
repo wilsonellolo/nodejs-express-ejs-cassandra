f61e629bb2906fb47a9420c07f26a61a733203fe
unknown <wilsonellolo@gmail.com>
2020-04-29 01:43:05 -0600

    consulta de datos simulacion de tickets terminada

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var cassandra= require('cassandra-driver');
-/*const path = require('path');
+const path = require('path');
 const bodyParser= require('body-parser');
 const express = require('express');
 
@@ -10,32 +10,10 @@ app.set('view engine','ejs' );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-app.get('/fecha',(req,res)=>{
-    res.render('fechas',{ cosas:"contenido"});
-});
-app.post('/fecha',(req,res)=>{
-    console.log(req.body.texto);
-    res.render('fechas',{ cosas:{hola:1,adios:'adios'}});
-});
-
-app.get('/usuario',(req,res)=>{
-    res.render('usuarioporfechas',{ cosas:{hola:1,adios:'adios'}});
-});
-app.post('/usuario',(req,res)=>{
-    console.log(req.body.texto);
-    res.render('usuarioporfechas',{ cosas:{hola:1,adios:'adios'}});
-});
-
-
-app.listen(app.get('port'),()=>{
-    console.log("servidor corriendo en puerto: "+app.get('port'));
-});
-*/
 var client= new cassandra.Client({
-    contactPoints: ['localhost'],
+    contactPoints: ['64.225.119.67'],
     localDataCenter: 'dcl'
-    });
+});
 
 client.connect(function(err,result){
     if(err){
@@ -45,17 +23,37 @@ client.connect(function(err,result){
     }
 });
 
-var get_todo="select * from prueba.nombre;";
 
-client.execute(get_todo,[],function(err,result){
-    if(err){
-        console.log('Hubo un error :('+err);
-    }else{
-        console.log(result.rows[2].titulo);
-        return;
-    }
+app.get('/fecha',(req,res)=>{
+    res.render('fechas',{result:[]});
+});
+app.post('/fecha',(req,res)=>{
+    var get_todo="select * from practica.ticket where fecha>='"+req.body.anioi+"-"+req.body.mesi+"-"+req.body.diai+"' and fecha<='"+req.body.aniof+"-"+req.body.mesf+"-"+req.body.diai+"'  allow filtering;";
+    client.execute(get_todo,[],function(err,result){
+        if(err){
+            console.log('Hubo un error :'+err);
+        }else{
+
+
+            res.render('fechas',{ result:result});        }
+    });
+
 });
 
-console.log("fin de la ejecucion");
+app.get('/usuario',(req,res)=>{
+    res.render('usuarioporfechas',{ result:[]});
+});
+app.post('/usuario',(req,res)=>{
+    var get_todo="select * from practica.ticket where correo='"+req.body.correo+"' and fecha>='"+req.body.anioi+"-"+req.body.mesi+"-"+req.body.diai+"' and fecha<='"+req.body.aniof+"-"+req.body.mesf+"-"+req.body.diai+"';";
+    client.execute(get_todo,[],function(err,result){
+        if(err){
+            console.log('Hubo un error :'+err);
+        }else{
+            res.render('usuarioporfechas',{ result:result});        
+        }
+    });
+});
 
-
+app.listen(app.get('port'),()=>{
+    console.log("servidor corriendo en puerto: "+app.get('port'));
+});
